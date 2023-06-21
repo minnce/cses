b8085e81e16d937fd7d15da7d07a5bf7fbe4db27
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-06-21 08:28:19 -0500

    Create distinctNumbers.cpp

---
+++ sortingSearching/distinctNumbers.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+int main()
+{
+    int n;
+    cin >> n;
+    set<int> s;
+    for (int i =0;i<n;i++) {
+        int d; cin >> d;
+        s.insert(d);
+    }
+    cout << s.size() << endl;
+}
+
